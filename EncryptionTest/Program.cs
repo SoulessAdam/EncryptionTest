82fc5d215cb2028077147a00704c009050f6c32c
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2021-04-08 16:44:08 +0100

    No

---
+++ EncryptionTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EncryptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
