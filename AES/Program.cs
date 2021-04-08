f0cfaaa09705f7437534356eb9916860c432d8c4
SoulessAdam <58148487+soulessadam@users.noreply.github.com>
2021-04-08 21:08:15 +0100

    Removed unneeded code.
    Signed-off-by: SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>

--- AES/Program.cs
+++ AES/Program.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.IO;
-using System.Threading.Tasks;
 using System.Security.Cryptography;
-using System.Linq;
+using System.Text;
 
 namespace EncryptionTest
 {
-    class Program
+    internal class Program
     {
         public static void Main()
         {
-            string a = "MinifiedScriptGoesHereCouldEvenUseJSFUCK?";
+            var a = "MinifiedScriptGoesHereCouldEvenUseJSFUCK?";
             using (Aes aes = new AesManaged())
             {
                 aes.KeySize = 256;
                 Console.WriteLine(aes.KeySize);
-                aes.Key = System.Text.Encoding.UTF8.GetBytes("FakeKeyForSendingToTheServerFun!"); /* RANDOMISE ON ACCOUNT CREATION, STORE IN DB, HAVE SENT ON LOGIN. LOAD INTO MEM TO ENCODE THEN WIPE!*/
+                aes.Key = Encoding.UTF8.GetBytes(
+                    "FakeKeyForSendingToTheServerFun!"); /* RANDOMISE ON ACCOUNT CREATION, STORE IN DB, HAVE SENT ON LOGIN. LOAD INTO MEM TO ENCODE THEN WIPE!*/
                 /* ^ Segment this serverside? Prevent MITM key stealing ig, more secure, client knows key, server knows key but only part of key is used, could even
                  BASE64 encode this incase too. -- Edit I have implemented this below, we can use this to secure our keys and make cracking this shit a pain
                  They'd have to first B64 decode the key, then try replicate out segments from our key. The actual key is never seen in plain text yet its easy for
@@ -23,10 +23,10 @@ namespace EncryptionTest
                  that I don't understand, we need to obfuscate and pack all of this shit too.  */
                 Console.WriteLine(aes.Key.Length);
                 // Implement this function to send off our key to a server and return our encrypted script
-                string base64Key = Convert.ToBase64String(aes.Key);
-                byte[] encrypted = EncryptStringToBytes_Aes(a, base64Key, aes.IV);
+                var base64Key = Convert.ToBase64String(aes.Key);
+                var encrypted = EncryptStringToBytes_Aes(a, base64Key, aes.IV);
                 // Decrypt the bytes returned from the function above to get out minified script
-                string decryted = DecryptStringFromBytes_Aes(encrypted, base64Key, aes.IV);
+                var decryted = DecryptStringFromBytes_Aes(encrypted, base64Key, aes.IV);
 
                 //Display the original data and the decrypted data.
                 Console.WriteLine($"Original:  {a}");
@@ -34,27 +34,31 @@ namespace EncryptionTest
             }
         }
 
-        static byte[] EncryptStringToBytes_Aes(string original, string Base64Key, byte[] IV)
+        private static byte[] EncryptStringToBytes_Aes(string original, string Base64Key, byte[] IV)
         {
             // This  would be serverside...
             byte[] encryptedScript;
-            using (AesManaged aesAlg = new AesManaged())
+            using (var aesAlg = new AesManaged())
             {
                 var Key = Convert.FromBase64String(Base64Key);
-                Byte[] actualKey = {Key[1], Key[3], Key[5], Key[16], Key[4], Key[2], Key[5], Key[1], Key[8], Key[31], Key[21], Key[16], Key[13], Key[15], Key[13], Key[0]};
+                byte[] actualKey =
+                {
+                    Key[1], Key[3], Key[5], Key[16], Key[4], Key[2], Key[5], Key[1], Key[8], Key[31], Key[21], Key[16],
+                    Key[13], Key[15], Key[13], Key[0]
+                };
                 aesAlg.KeySize = 128;
                 aesAlg.Key = actualKey;
                 aesAlg.IV = IV;
 
                 // Create an encryptor to perform the stream transform.
-                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
 
                 // Create the streams used for encryption.
-                using (MemoryStream msEncrypt = new MemoryStream())
+                using (var msEncrypt = new MemoryStream())
                 {
-                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
                     {
-                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
                         {
                             //Write all data to the stream.
                             swEncrypt.Write(original);
@@ -64,33 +68,37 @@ namespace EncryptionTest
                     }
                 }
             }
+
             // Send data back to client
             return encryptedScript;
         }
 
-        static string DecryptStringFromBytes_Aes(byte[] encrypted, string Base64Key, byte[] IV)
+        private static string DecryptStringFromBytes_Aes(byte[] encrypted, string Base64Key, byte[] IV)
         {
             // create a string for our minified script.
-            string script = "";
+            var script = "";
 
-            using (AesManaged aesAlg = new AesManaged())
+            using (var aesAlg = new AesManaged())
             {
-                byte[] Key = Convert.FromBase64String(Base64Key);
-                Byte[] actualKey = {Key[1], Key[3], Key[5], Key[16], Key[4], Key[2], Key[5], Key[1], Key[8], Key[31], Key[21], Key[16], Key[13], Key[15], Key[13], Key[0]};
+                var Key = Convert.FromBase64String(Base64Key);
+                byte[] actualKey =
+                {
+                    Key[1], Key[3], Key[5], Key[16], Key[4], Key[2], Key[5], Key[1], Key[8], Key[31], Key[21], Key[16],
+                    Key[13], Key[15], Key[13], Key[0]
+                };
                 aesAlg.Key = actualKey;
                 aesAlg.IV = IV;
 
                 // Create a decryptor to perform the stream transform.
-                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
 
                 // Create the streams used for decryption.
-                using (MemoryStream msDecrypt = new MemoryStream(encrypted))
+                using (var msDecrypt = new MemoryStream(encrypted))
                 {
-                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
                     {
-                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        using (var srDecrypt = new StreamReader(csDecrypt))
                         {
-
                             // Read the decrypted bytes from the decrypting stream
                             // and place them in a string.
                             script = srDecrypt.ReadToEnd();
@@ -98,6 +106,7 @@ namespace EncryptionTest
                     }
                 }
             }
+
             return script;
         }
     }
