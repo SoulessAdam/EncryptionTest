d033cfa3e7c91eb506bcaeca14791936d9248d50
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2021-05-18 20:28:23 +0100

    First alpha build
    Signed-off-by: SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>

---
+++ ImageStorage/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageStorage
+{
+    internal class Program
+    {
+        private static string Key = "TheDefaultKeyPleaseOverwriteThis";
+
+        // private static string Mode;
+        private static int FileCount = Directory.EnumerateFiles(Settings.savePath).ToArray().Length;
+
+        private static async Task Main(string[] args)
+        {
+            await Startup.checkDir();
+            Key = args[2];
+            EncryptImages();
+            // CHECK MODES FOR DECRYPT WHEN DONE
+        }
+
+        private static Image[] EnumerateImages()
+        {
+            var dir = new DirectoryInfo(Directory.GetCurrentDirectory());
+            var files = dir.GetFiles("*.png");
+            var images = new List<Image>();
+            foreach (var image in files) images.Add(Image.FromFile(image.DirectoryName));
+
+            return images.ToArray();
+        }
+
+        private static FileInfo[] EnumerateEncrypts()
+        {
+            var dir = new DirectoryInfo(Directory.GetCurrentDirectory());
+            var files = dir.GetFiles("*.ae");
+            return files;
+            // TODO: FINISH OFF DECRYPTING FOR INBUILT IMAGE VIEWING. CONVERT TO WINFORMS/WPF AT SOME POINT
+        }
+
+        private static void EncryptImages()
+        {
+            foreach (var image in EnumerateImages())
+                using (var m = new MemoryStream())
+                {
+                    image.Save(m, image.RawFormat);
+                    EncryptBytes(m.ToArray());
+                }
+        }
+
+        private static async void EncryptBytes(byte[] bytes)
+        {
+            using (var aesAlg = new AesManaged())
+            {
+                aesAlg.KeySize = 256;
+                aesAlg.Key = Encoding.UTF8.GetBytes(Key);
+
+                var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(bytes);
+                        }
+
+                        await SaveImage(msEncrypt.ToArray(), $"{FileCount++}.ae");
+                    }
+                }
+            }
+        }
+
+        private static Task SaveImage(byte[] encryptedBytes, string name = "you fucked up.ae")
+        {
+            File.WriteAllBytes($"{Settings.savePath}/{name}", encryptedBytes);
+            return Task.CompletedTask;
+        }
+    }
+}
